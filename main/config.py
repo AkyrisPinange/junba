64d120239564a3403ae3766d348f5c8753efefc8
AkyrisSales <48262372+AkyrisPinange@users.noreply.github.com>
2025-05-30 14:37:51 -0300

    d

--- main/config.py
+++ main/config.py
@@ -1,11 +1,8 @@
 from datetime import datetime
 import os
-from dotenv import load_dotenv
 
-load_dotenv()
-
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-OPENROUTER_API_KEY = os.getenv("OPENROUTER_API_KEY")
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+OPENROUTER_API_KEY = os.environ["OPENROUTER_API_KEY"]
 START_DATE = datetime(2025, 4, 4)
 CANAL_ID_PICANHA = 1357801507302408314
 CANAL_ID_CHAT = 1378046129341468715
