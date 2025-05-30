61e4a806a45981ae52b270140d69ca2f7a53f02a
AkyrisSales <48262372+AkyrisPinange@users.noreply.github.com>
2025-05-30 16:20:52 -0300

    ddss

--- bot.py
+++ bot.py
@@ -2,11 +2,25 @@ import discord
 from discord.ext import commands
 import asyncio
 from main.config import BOT_PREFIX, BOT_TOKEN
+import threading
+from flask import Flask
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix=BOT_PREFIX, intents=intents)
+app = Flask("junba")
+
+@app.route("/")
+def home():
+    return "Junba vivor, veior!"
+
+def run_web():
+    app.run(host="0.0.0.0", port=8080)
+
+# Roda o servidor Flask em paralelo
+threading.Thread(target=run_web).start()
+
 
 @bot.event
 async def on_ready():
